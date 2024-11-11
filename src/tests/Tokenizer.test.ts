62930a7de2a1ad1943b44995605fa682667b742f
Victor Azevedo <victor.azevedo@mail.com>
2024-11-11 09:45:18 +0100

    Trying to make tests work without vitest

--- src/tests/Tokenizer.test.ts
+++ src/tests/Tokenizer.test.ts
@@ -1,6 +1,6 @@
-import { describe, expect, it } from 'vitest'
+import { expect } from '@std/expect'
 import { Tokenizer } from '../index.ts'
-import type { Callbacks } from '../Tokenizer.ts'
+import type { TokenizerCallbacks } from '../Tokenizer.ts'
 
 function tokenize(data: string, options = {}) {
 	const log: unknown[][] = []
@@ -13,7 +13,7 @@ function tokenize(data: string, options = {}) {
 					return (...values: unknown[]) => log.push([property, ...values])
 				},
 			},
-		) as Callbacks,
+		) as TokenizerCallbacks,
 	)
 
 	tokenizer.write(data)
@@ -22,123 +22,103 @@ function tokenize(data: string, options = {}) {
 	return log
 }
 
-describe('Tokenizer', () => {
-	describe('should support self-closing special tags', () => {
-		it('for self-closing script tag', () => {
-			expect(tokenize('<script /><div></div>')).toMatchSnapshot()
-		})
-		it('for self-closing style tag', () => {
-			expect(tokenize('<style /><div></div>')).toMatchSnapshot()
-		})
-		it('for self-closing title tag', () => {
-			expect(tokenize('<title /><div></div>')).toMatchSnapshot()
-		})
-		it('for self-closing textarea tag', () => {
-			expect(tokenize('<textarea /><div></div>')).toMatchSnapshot()
-		})
-	})
+Deno.test('Self-closing special tags: for self-closing script tag', () => {
+	expect(tokenize('<script /><div></div>')).toMatchSnapshot()
+})
+Deno.test('Self-closing special tags: for self-closing style tag', () => {
+	expect(tokenize('<style /><div></div>')).toMatchSnapshot()
+})
+Deno.test('Self-closing special tags: for self-closing title tag', () => {
+	expect(tokenize('<title /><div></div>')).toMatchSnapshot()
+})
+Deno.test('Self-closing special tags: for self-closing textarea tag', () => {
+	expect(tokenize('<textarea /><div></div>')).toMatchSnapshot()
+})
 
-	describe('should support standard special tags', () => {
-		it('for normal script tag', () => {
-			expect(tokenize('<script></script><div></div>')).toMatchSnapshot()
-		})
-		it('for normal style tag', () => {
-			expect(tokenize('<style></style><div></div>')).toMatchSnapshot()
-		})
-		it('for normal sitle tag', () => {
-			expect(tokenize('<title></title><div></div>')).toMatchSnapshot()
-		})
-		it('for normal textarea tag', () => {
-			expect(tokenize('<textarea></textarea><div></div>')).toMatchSnapshot()
-		})
-	})
+Deno.test('Standard special tags: for normal script tag', () => {
+	expect(tokenize('<script></script><div></div>')).toMatchSnapshot()
+})
+Deno.test('Standard special tags: for normal style tag', () => {
+	expect(tokenize('<style></style><div></div>')).toMatchSnapshot()
+})
+Deno.test('Standard special tags: for normal sitle tag', () => {
+	expect(tokenize('<title></title><div></div>')).toMatchSnapshot()
+})
+Deno.test('Standard special tags: for normal textarea tag', () => {
+	expect(tokenize('<textarea></textarea><div></div>')).toMatchSnapshot()
+})
 
-	describe('should treat html inside special tags as text', () => {
-		it('for div inside script tag', () => {
-			expect(tokenize('<script><div></div></script>')).toMatchSnapshot()
-		})
-		it('for div inside style tag', () => {
-			expect(tokenize('<style><div></div></style>')).toMatchSnapshot()
-		})
-		it('for div inside title tag', () => {
-			expect(tokenize('<title><div></div></title>')).toMatchSnapshot()
-		})
-		it('for div inside textarea tag', () => {
-			expect(tokenize('<textarea><div></div></textarea>')).toMatchSnapshot()
-		})
-	})
+Deno.test('Html inside special tags as text: for div inside script tag', () => {
+	expect(tokenize('<script><div></div></script>')).toMatchSnapshot()
+})
+Deno.test('Html inside special tags as text: for div inside style tag', () => {
+	expect(tokenize('<style><div></div></style>')).toMatchSnapshot()
+})
+Deno.test('Html inside special tags as text: for div inside title tag', () => {
+	expect(tokenize('<title><div></div></title>')).toMatchSnapshot()
+})
+Deno.test('Html inside special tags as text: for div inside textarea tag', () => {
+	expect(tokenize('<textarea><div></div></textarea>')).toMatchSnapshot()
+})
 
-	describe('should correctly mark attributes', () => {
-		it('for no value attribute', () => {
-			expect(tokenize('<div aaaaaaa >')).toMatchSnapshot()
-		})
-		it('for no quotes attribute', () => {
-			expect(tokenize('<div aaa=aaa >')).toMatchSnapshot()
-		})
-		it('for single quotes attribute', () => {
-			expect(tokenize("<div aaa='a' >")).toMatchSnapshot()
-		})
-		it('for double quotes attribute', () => {
-			expect(tokenize('<div aaa="a" >')).toMatchSnapshot()
-		})
-	})
+Deno.test('Mark attributes: for no value attribute', () => {
+	expect(tokenize('<div aaaaaaa >')).toMatchSnapshot()
+})
+Deno.test('Mark attributes: for no quotes attribute', () => {
+	expect(tokenize('<div aaa=aaa >')).toMatchSnapshot()
+})
+Deno.test('Mark attributes: for single quotes attribute', () => {
+	expect(tokenize("<div aaa='a' >")).toMatchSnapshot()
+})
+Deno.test('Mark attributes: for double quotes attribute', () => {
+	expect(tokenize('<div aaa="a" >')).toMatchSnapshot()
+})
 
-	describe('should not break after special tag followed by an entity', () => {
-		it('for normal special tag', () => {
-			expect(tokenize('<style>a{}</style>&apos;<br/>')).toMatchSnapshot()
-		})
-		it('for self-closing special tag', () => {
-			expect(tokenize('<style />&apos;<br/>')).toMatchSnapshot()
-		})
-	})
+Deno.test('Not break after special tag followed by an entity: for normal special tag', () => {
+	expect(tokenize('<style>a{}</style>&apos;<br/>')).toMatchSnapshot()
+})
+Deno.test('Not break after special tag followed by an entity: for self-closing special tag', () => {
+	expect(tokenize('<style />&apos;<br/>')).toMatchSnapshot()
+})
 
-	describe('should handle entities', () => {
-		it('for XML entities', () =>
-			expect(
-				tokenize('&amp;&gt;&amp&lt;&uuml;&#x61;&#x62&#99;&#100&#101', {
-					xmlMode: true,
-				}),
-			).toMatchSnapshot())
+Deno.test('Handle entities: for entities in attributes (#276)', () =>
+	expect(
+		tokenize('<img src="?&image_uri=1&&image;=2&image=3"/>?&image_uri=1&&image;=2&image=3'),
+	).toMatchSnapshot())
 
-		it('for entities in attributes (#276)', () =>
-			expect(
-				tokenize('<img src="?&image_uri=1&&image;=2&image=3"/>?&image_uri=1&&image;=2&image=3'),
-			).toMatchSnapshot())
+Deno.test('Handle entities: for trailing legacy entity', () =>
+	expect(tokenize('&timesbar;&timesbar')).toMatchSnapshot())
 
-		it('for trailing legacy entity', () => expect(tokenize('&timesbar;&timesbar')).toMatchSnapshot())
+Deno.test('Handle entities: for multi-byte entities', () => expect(tokenize('&NotGreaterFullEqual;')).toMatchSnapshot())
 
-		it('for multi-byte entities', () => expect(tokenize('&NotGreaterFullEqual;')).toMatchSnapshot())
-	})
-
-	it('should not lose data when pausing', () => {
-		const log: unknown[][] = []
-		const tokenizer = new Tokenizer(
+Deno.test('should not lose data when pausing', () => {
+	const log: unknown[][] = []
+	const tokenizer = new Tokenizer(
+		{},
+		new Proxy(
 			{},
-			new Proxy(
-				{},
-				{
-					get(_, property) {
-						return (...values: unknown[]) => {
-							if (property === 'ontext') {
-								tokenizer.pause()
-							}
-							log.push([property, ...values])
+			{
+				get(_, property) {
+					return (...values: unknown[]) => {
+						if (property === 'ontext') {
+							tokenizer.pause()
 						}
-					},
+						log.push([property, ...values])
+					}
 				},
-			) as Callbacks,
-		)
+			},
+		) as TokenizerCallbacks,
+	)
 
-		tokenizer.write('&am')
-		tokenizer.write('p; it up!')
-		tokenizer.resume()
-		tokenizer.resume()
+	tokenizer.write('&am')
+	tokenizer.write('p; it up!')
+	tokenizer.resume()
+	tokenizer.resume()
 
-		// Tokenizer shouldn't be paused
-		expect(tokenizer).toHaveProperty('running', true)
+	// Tokenizer shouldn't be paused
+	expect(tokenizer).toHaveProperty('running', true)
 
-		tokenizer.end()
+	tokenizer.end()
 
-		expect(log).toMatchSnapshot()
-	})
+	expect(log).toMatchSnapshot()
 })
