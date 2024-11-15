8d783ad9f44515bd5ae9e220d7834a89b739075f
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2024-11-15 09:32:09 +0100

    Format entities

--- src/benches/index.bench.ts
+++ src/benches/index.bench.ts
@@ -3,36 +3,36 @@ import { Parser } from '../index.ts'
 const html = await (await fetch('https://accuweather.com/')).text()
 
 Deno.bench('No options', function () {
-    let text = ''
-    let attr = ''
+	let text = ''
+	let attr = ''
 
-    const parser = new Parser({
-        ontext(data) {
-            text += data
-        },
-        onattribute(data) {
-            attr += data
-        },
-        onend() {
-            console.log(text)
-        },
-    })
+	const parser = new Parser({
+		ontext(data) {
+			text += data
+		},
+		onattribute(data) {
+			attr += data
+		},
+		onend() {
+			console.log(text)
+		},
+	})
 
-    parser.parseComplete(html)
+	parser.parseComplete(html)
 })
 
 Deno.bench('No decoding', function () {
-    let text = ''
-    let attr = ''
+	let text = ''
+	let attr = ''
 
-    const parser = new Parser({
-        ontext(data) {
-            text += data
-        },
-        onattribute(data) {
-            attr += data
-        },
-    }, { decodeEntities: false })
+	const parser = new Parser({
+		ontext(data) {
+			text += data
+		},
+		onattribute(data) {
+			attr += data
+		},
+	}, { decodeEntities: false })
 
-    parser.parseComplete(html)
+	parser.parseComplete(html)
 })
--- src/entities/decode-codepoint.ts
+++ src/entities/decode-codepoint.ts
@@ -1,57 +1,57 @@
 // Adapted from https://github.com/mathiasbynens/he/blob/36afe179392226cf1b6ccdb16ebbb7a5a844d93a/src/he.js#L106-L134
 
 const decodeMap = new Map([
-    [0, 65_533],
-    // C1 Unicode control character reference replacements
-    [128, 8364],
-    [130, 8218],
-    [131, 402],
-    [132, 8222],
-    [133, 8230],
-    [134, 8224],
-    [135, 8225],
-    [136, 710],
-    [137, 8240],
-    [138, 352],
-    [139, 8249],
-    [140, 338],
-    [142, 381],
-    [145, 8216],
-    [146, 8217],
-    [147, 8220],
-    [148, 8221],
-    [149, 8226],
-    [150, 8211],
-    [151, 8212],
-    [152, 732],
-    [153, 8482],
-    [154, 353],
-    [155, 8250],
-    [156, 339],
-    [158, 382],
-    [159, 376],
-]);
+	[0, 65_533],
+	// C1 Unicode control character reference replacements
+	[128, 8364],
+	[130, 8218],
+	[131, 402],
+	[132, 8222],
+	[133, 8230],
+	[134, 8224],
+	[135, 8225],
+	[136, 710],
+	[137, 8240],
+	[138, 352],
+	[139, 8249],
+	[140, 338],
+	[142, 381],
+	[145, 8216],
+	[146, 8217],
+	[147, 8220],
+	[148, 8221],
+	[149, 8226],
+	[150, 8211],
+	[151, 8212],
+	[152, 732],
+	[153, 8482],
+	[154, 353],
+	[155, 8250],
+	[156, 339],
+	[158, 382],
+	[159, 376],
+])
 
 /**
  * Polyfill for `String.fromCodePoint`. It is used to create a string from a Unicode code point.
  */
 export const fromCodePoint: (...codePoints: number[]) => string =
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, n/no-unsupported-features/es-builtins
-    String.fromCodePoint ??
-    function (codePoint: number): string {
-        let output = "";
+	// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition, n/no-unsupported-features/es-builtins
+	String.fromCodePoint ??
+		function (codePoint: number): string {
+			let output = ''
 
-        if (codePoint > 0xff_ff) {
-            codePoint -= 0x1_00_00;
-            output += String.fromCharCode(
-                ((codePoint >>> 10) & 0x3_ff) | 0xd8_00,
-            );
-            codePoint = 0xdc_00 | (codePoint & 0x3_ff);
-        }
+			if (codePoint > 0xff_ff) {
+				codePoint -= 0x1_00_00
+				output += String.fromCharCode(
+					((codePoint >>> 10) & 0x3_ff) | 0xd8_00,
+				)
+				codePoint = 0xdc_00 | (codePoint & 0x3_ff)
+			}
 
-        output += String.fromCharCode(codePoint);
-        return output;
-    };
+			output += String.fromCharCode(codePoint)
+			return output
+		}
 
 /**
  * Replace the given code point with a replacement character if it is a
@@ -59,14 +59,14 @@ export const fromCodePoint: (...codePoints: number[]) => string =
  * point unchanged.
  */
 export function replaceCodePoint(codePoint: number): number {
-    if (
-        (codePoint >= 0xd8_00 && codePoint <= 0xdf_ff) ||
-        codePoint > 0x10_ff_ff
-    ) {
-        return 0xff_fd;
-    }
+	if (
+		(codePoint >= 0xd8_00 && codePoint <= 0xdf_ff) ||
+		codePoint > 0x10_ff_ff
+	) {
+		return 0xff_fd
+	}
 
-    return decodeMap.get(codePoint) ?? codePoint;
+	return decodeMap.get(codePoint) ?? codePoint
 }
 
 /**
@@ -77,5 +77,5 @@ export function replaceCodePoint(codePoint: number): number {
  * @returns The decoded code point.
  */
 export function decodeCodePoint(codePoint: number): string {
-    return fromCodePoint(replaceCodePoint(codePoint));
+	return fromCodePoint(replaceCodePoint(codePoint))
 }
